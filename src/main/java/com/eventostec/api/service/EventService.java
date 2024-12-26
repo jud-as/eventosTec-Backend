aca7d35a4d2bfed499a561d9b982ab3f42d8ec84
jud-as <guilhermevalente.dev@gmail.com>
2024-12-25 22:12:59 -0300

    Fixing uploadImg return

--- src/main/java/com/eventostec/api/service/EventService.java
+++ src/main/java/com/eventostec/api/service/EventService.java
@@ -57,7 +57,7 @@ public class EventService {
             return s3Client.getUrl(bucketName, fileName).toString();
         } catch (Exception e) {
             System.out.println("erro ao subir arquivo.");
-            return "";
+            return null;
         }
     }
 
