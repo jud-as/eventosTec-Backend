8be19470a8d34a7f14bedd8484fc3883940b9bfd
jud-as <guilhermevalente.dev@gmail.com>
2024-12-26 13:39:18 -0300

    Created Event Controller for page list

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -2,14 +2,17 @@ package com.eventostec.api.controller;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
+import java.util.List;
 
 import static com.fasterxml.jackson.databind.type.LogicalType.DateTime;
 
@@ -33,5 +36,11 @@ public class EventController {
         EventRequestDTO eventData = new EventRequestDTO(title, description, date, city, uf, remote, eventUrl, image);
         Event newEvent = eventService.createEvent(eventData);
         return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        List<EventResponseDTO> allEvents = eventService.getEvents(page, size);
+        return ResponseEntity.ok(allEvents);
     }
 }
