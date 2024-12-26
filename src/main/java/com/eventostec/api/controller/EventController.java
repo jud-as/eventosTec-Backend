46be23da5f7b02fa7fd5436408463db32673a02a
jud-as <guilhermevalente.dev@gmail.com>
2024-12-26 13:55:01 -0300

    List upcoming events

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -40,7 +40,7 @@ public class EventController {
 
     @GetMapping
     public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
-        List<EventResponseDTO> allEvents = eventService.getEvents(page, size);
+        List<EventResponseDTO> allEvents = eventService.getUpComingEvents(page, size);
         return ResponseEntity.ok(allEvents);
     }
 }
