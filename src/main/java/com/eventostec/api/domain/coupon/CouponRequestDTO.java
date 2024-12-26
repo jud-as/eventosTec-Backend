b60052e235ec0bd3ddd7f3e81fb176faecf0b0ec
jud-as <guilhermevalente.dev@gmail.com>
2024-12-25 22:17:31 -0300

    Creating CouponRequestDTO

--- src/main/java/com/eventostec/api/domain/coupon/CouponRequestDTO.java
+++ src/main/java/com/eventostec/api/domain/coupon/CouponRequestDTO.java
@@ -1,4 +1,8 @@
 package com.eventostec.api.domain.coupon;
 
-public class CouponRequestDTO {
+import com.eventostec.api.domain.event.Event;
+
+import java.util.Date;
+
+public record CouponRequestDTO (Integer discount, Date date, Event event){
 }
