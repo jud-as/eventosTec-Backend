df8fa43d33364be73e74fba24fe8259b623db85a
jud-as <guilhermevalente.dev@gmail.com>
2024-12-25 22:13:43 -0300

    Creating Coupon functionality structure

---
+++ src/main/java/com/eventostec/api/controller/CouponController.java
@@ -0,0 +1,24 @@
+package com.eventostec.api.controller;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/coupon")
+public class CouponController {
+
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping("/event/{eventId}")
+    public ResponseEntity<Coupon> addCouponToEvent(@PathVariable UUID eventId, @RequestBody CouponRequestDTO couponData) {
+        return ResponseEntity.ok().build();
+    }
+
+}
