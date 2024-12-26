60ac43ea616948e1c031b2e9d1c7214a72a435bd
jud-as <guilhermevalente.dev@gmail.com>
2024-12-25 22:43:47 -0300

    Creating addCouponToEvent Function

--- src/main/java/com/eventostec/api/service/CouponService.java
+++ src/main/java/com/eventostec/api/service/CouponService.java
@@ -1,7 +1,37 @@
 package com.eventostec.api.service;
 
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.repository.CouponRepository;
+import com.eventostec.api.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponData) {
+
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        Coupon coupon = new Coupon();
+        coupon.setCode(couponData.code());
+        coupon.setDiscount(couponData.discount());
+        coupon.setValid(couponData.valid());
+        coupon.setEvent(event);
+
+        return couponRepository.save(coupon);
+    }
+
+
 }
