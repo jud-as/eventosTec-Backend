3b10adb16daa5a66380ed3f30d7103da2b475942
jud-as <guilhermevalente.dev@gmail.com>
2024-12-25 22:44:16 -0300

    Fixing CouponRequestDTO params

--- src/main/java/com/eventostec/api/domain/coupon/CouponRequestDTO.java
+++ src/main/java/com/eventostec/api/domain/coupon/CouponRequestDTO.java
@@ -4,5 +4,5 @@ import com.eventostec.api.domain.event.Event;
 
 import java.util.Date;
 
-public record CouponRequestDTO (Integer discount, Date date, Event event){
+public record CouponRequestDTO (String code, Integer discount, Long valid){
 }
